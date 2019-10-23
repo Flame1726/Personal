13e5abc5fdc27349ab9436cc9ade268cca814845
Francis Ynoa <francisynoa@yahoo.com>
2019-10-23 00:07:11 -0400

    yo

---
+++ app/src/main/java/com/example/ZombieSlayer/Controller/MainMenuController.java
@@ -0,0 +1,21 @@
+package com.example.ZombieSlayer.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ZombieSlayer.R;
+import com.example.ZombieSlayer.Views.LoadView;
+
+public class MainMenuController extends AppCompatActivity {
+
+    private LoadView load;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
--- app/src/main/java/com/example/ZombieSlayer/Views/WindowView.java
+++ app/src/main/java/com/example/ZombieSlayer/Views/WindowView.java
@@ -10,7 +10,7 @@ public class WindowView {
     public void hideSystem(AppCompatActivity _window) {
         View decorView = _window.getWindow().getDecorView();
         decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         // Set the content to appear under the system bars so that the
                         // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
